98a96d0efdf156b3806721164efef822d419e400
gek0z <riccardoaltieri@me.com>
2025-01-27 12:25:31 +0000

    fix: static og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import { TRPCReactProvider } from "~/trpc/react";
 import { Toaster } from "~/components/ui/toaster";
 import { defaultMetadata } from "~/config/metadata";
-import { Metadata } from "next";
 
 import GoogleAnalytics from "~/components/GoogleAnalytics";
 
@@ -12,15 +11,23 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
-export const metadata: Metadata = {
-  ...defaultMetadata,
-};
+export const metadata = defaultMetadata;
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${inter.variable}`}>
+      <head>
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/wedgietracker/image/upload/v1736700345/assets/social-wedgietracker_bibnbu.jpg"
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/wedgietracker/image/upload/v1736700345/assets/social-wedgietracker_bibnbu.jpg"
+        />
+      </head>
       <body className="bg-darkpurple">
         <TRPCReactProvider>
           {children}
