6519971281b06e1f85d2625ea90df28ce318b781
gek0z <riccardoaltieri@me.com>
2025-05-05 13:52:11 +0100

    fix: ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export const metadata = defaultMetadata;
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const gtmId = process.env.NEXT_PUBLIC_GA_ID!;
+
   return (
     <html lang="en" className={`${inter.variable}`}>
       <head>
@@ -29,11 +31,22 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-darkpurple">
+        {/* GTM noscript iframe for users with JavaScript disabled */}
+        {/* <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="gtm"
+          />
+        </noscript> */}
+
         <TRPCReactProvider>
           {children}
           <Toaster />
 
-          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
+          <GoogleAnalytics gaId={gtmId} />
         </TRPCReactProvider>
       </body>
     </html>
